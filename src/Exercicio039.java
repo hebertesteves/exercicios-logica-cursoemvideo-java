512c43e19588720902a641cc482899cc91c137e8
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-06-25 21:16:21 -0300

    Adicionando exercício 039

---
+++ src/Exercicio039.java
@@ -0,0 +1,8 @@
+public class Exercicio039 {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 3; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.print("Acabou!");
+    }
+}
