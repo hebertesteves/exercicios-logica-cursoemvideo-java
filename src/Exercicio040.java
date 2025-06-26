cd72a63e6a8cbe248c1efc04895952e136f63bf2
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-06-25 21:16:26 -0300

    Adicionando exercício 040

---
+++ src/Exercicio040.java
@@ -0,0 +1,8 @@
+public class Exercicio040 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 18; i += 3) {
+            System.out.print(i + " ");
+        }
+        System.out.print("Acabou!");
+    }
+}
