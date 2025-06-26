8ded8dd5248e48eee47b8abfb564ac693b3f93b7
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-06-25 21:16:17 -0300

    Adicionando exercício 038

---
+++ src/Exercicio038.java
@@ -0,0 +1,8 @@
+public class Exercicio038 {
+    public static void main(String[] args) {
+        for (int i = 6; i <= 11; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.print("Acabou!");
+    }
+}
