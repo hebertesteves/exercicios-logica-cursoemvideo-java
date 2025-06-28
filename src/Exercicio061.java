1e79c681ceaab9f4d987b68ad37eb99ed77fc6fa
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-06-28 16:05:19 -0300

    Adicionando exercício 061

---
+++ src/Exercicio061.java
@@ -0,0 +1,12 @@
+public class Exercicio061 {
+    public static void main(String[] args) {
+        int i = 0;
+
+        do {
+            System.out.print(i + " ");
+            i += 3;
+        } while (i <= 30);
+
+        System.out.print("Acabou!");
+    }
+}
