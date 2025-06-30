fdd284cc1e278a5db80ddbbdfa0929670a08eb01
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-06-30 12:52:59 -0300

    Adicionando exercício 087

---
+++ src/Exercicio087.java
@@ -0,0 +1,11 @@
+public class Exercicio087 {
+    public static void main(String[] args) {
+        gerador("Aprendendo Portugol");
+    }
+
+    public static void gerador(String texto) {
+        System.out.println("+-------=======------+");
+        System.out.println("  " + texto);
+        System.out.println("+-------=======------+");
+    }
+}
